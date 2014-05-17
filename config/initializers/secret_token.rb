c365ab34a6898b6f25a5e0a0d254683ec5c827ba
jcbeaury <jcbeaury@gmail.com>
2014-05-17 02:53:32 -0400

    Initialize rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaffleBackbonejs::Application.config.secret_key_base = 'f2c0a79c4565160068cab74f06f61745dabe0d2f517669b4693922402eeca11ec22b29dbb1684ee5faee0e091c9a16d17cc1488f4df4b474ffe2d5d16063ab19'
