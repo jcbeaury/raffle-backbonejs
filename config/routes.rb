004f1c97075b4a49cf6261f729b4aab3d9314f99
jcbeaury <jcbeaury@gmail.com>
2014-05-17 05:35:21 -0400

    Add seed data and json output

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 RaffleBackbonejs::Application.routes.draw do
-  resources :entries
+
+  scope "api" do
+    resources :entries
+  end
 
   root to: "main#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
